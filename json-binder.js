b6c81e6c5aa57b5fa9c6c7ea37b51e4277ae5af7
Andre Taddeini <ataddeini@gmail.com>
2012-06-19 22:18:01 -0500

    Creating tests and binding for checkbox and radios

--- json-binder.js
+++ json-binder.js
@@ -1,4 +1,60 @@
 ﻿(function ($) {
-  $.fn.JSONBind() = function () {
+  // Namespace for input binding definitions
+  var binding = {};
+
+  // Binding definition for checkboxes
+  binding.checkbox = function ($checkbox) {
+    var _name = $checkbox.attr("name"),
+        _hasValue = typeof _name !== "undefined",
+        _value = $checkbox.is(":checked") ? true : false;
+
+    return {
+      name: _name,
+      hasValue: _hasValue,
+      value: _value
+    }
+  };
+
+  /// Binding definition for radios
+  binding.radio = function ($radio) {
+    var _name = $radio.attr("name"),
+        _hasValue = typeof _name !== "undefined",
+        _value = $radio.is(":checked") ? $radio.val() : "";
+
+    return {
+      name: _name,
+      hasValue: _hasValue,
+      value: _value
+    }
+  };
+
+  $.fn.JSONBind = function () {
+    // Stores the JSON object to be returned
+    var result = {},
+    // Array that will hold any input bindings that need to be run
+    active_bindings = [],
+    // Gets the jQuery items for any given inputs
+    $inputs = this.find("input");
+
+    // For each input, determine if a binding definition exists, and if it
+    // does create and store it
+    $inputs.each(function () {
+      var $this = $(this);
+      var binder = binding[$this.attr("type")];
+
+      if (typeof binder !== "undefined") {
+        active_bindings.push(binder($this));
+      }
+    });
+
+    // For any binding definitions that were created, run them to get the
+    // input data to the raw JSON values
+    $.each(active_bindings, function (index, binder) {
+      if (binder.hasValue) {
+        result[binder.name] = binder.value;
+      }
+    });
+    
+    return result;
   };
 })(jQuery);
--- tests/json-binder.tests.js
+++ tests/json-binder.tests.js
@@ -1,1 +1,57 @@
-﻿
+﻿// CHECKBOX
+module("checkbox", {});
+
+test("A selected checkbox without a name should return no value", function () {
+  var input = "<form><input type='checkbox' checked='checked'>Foo</input></form>";
+  var model = $(input).JSONBind();
+  strictEqual(Object.keys(model).length, 0);
+});
+
+test("A de-selected checkbox should bind to a false property value", function () {
+  var input = "<form><input type='checkbox' name='foo'>Foo</input></form>";
+  var model = $(input).JSONBind();
+  strictEqual(model.foo, false);
+});
+
+test("A selected checkbox should bind to a true property value", function () {
+  var input = "<form><input type='checkbox' checked='checked' name='foo'>Foo</input></form>";
+  var model = $(input).JSONBind();
+  strictEqual(model.foo, true);
+});
+
+//RADIO
+module("checkbox", {});
+
+test("A radio without a name should return no value", function () {
+  var input =
+    "<form>" +
+      "<input type='radio' value='bar' checked='checked'/>" +
+      "<input type='radio' value='fizz' />" +
+    "</form>";
+  var model = $(input).JSONBind();
+  strictEqual(Object.keys(model).length, 0);
+});
+
+test("A radio with no selections should return an empty value", function () {
+  var input =
+    "<form>" +
+      "<input type='radio' name='foo' value='bar' />" +
+      "<input type='radio' name='foo' value='fizz' />" +
+    "</form>";
+  var model = $(input).JSONBind();
+  strictEqual(model.foo, '');
+});
+
+test("A selected checkbox should bind to a true property value", function () {
+  var input =
+    "<form>" +
+      "<input type='radio' name='foo' value='bar' />" +
+      "<input type='radio' name='foo' value='fizz' checked='checked'/>" +
+    "</form>";
+  var model = $(input).JSONBind();
+  strictEqual(model.foo, 'fizz');
+});
+
+// HIDDEN
+// TEXT/PASSWORD
+// SELECT (SINGLE & MULTI)
